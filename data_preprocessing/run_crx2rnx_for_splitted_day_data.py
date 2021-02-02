a080a7ac7c30232bc36e73d2ec4fbaec15a397be
kelemensz <Szabolcs.Kelemen@arrk-engineering.com>
2021-02-02 18:22:49 +0200

    Run convbin for HKG data updated

--- data_preprocessing/run_crx2rnx_for_splitted_day_data.py
+++ data_preprocessing/run_crx2rnx_for_splitted_day_data.py
@@ -1,46 +1,39 @@
 import os
-from datetime import datetime 
-
+from datetime import datetime
 
 
 def get_subfolders_with_source(root_path):
     return [os.path.join(root_path, f) for f in os.listdir(root_path) if os.path.isdir(os.path.join(root_path, f))]
 
 
-
 def run_CRX2RNX(day_dir):
     day_index = os.path.split(day_dir)[-1]
+    # zipped_files = str(os.path.normpath(os.path.join(day_dir, "*.gz")))
+    # unzipp = "gunzip " + zipped_files
+    # os.system(unzipp)
+    input_files = [os.path.join(day_dir, f) for f in os.listdir(day_dir) if
+                   (os.path.isfile(os.path.join(day_dir, f)) and str(f)[-2:] == "0d")]
 
-    input_files = [os.path.join(day_dir, f) for f in os.listdir(day_dir) if (os.path.isfile(os.path.join(day_dir, f)) and  str(f)[-2:] != "gz" )]
-
-    
-    for file in input_files:
-        command1 = "./CRX2RNX {} -f -d".format(file)
-        
+    for file in input_files:  # [:1]:
+        file = os.path.normpath(file)
+        tool = os.path.normpath(r"C:\SzabolcsKelemen\PhD\GPS\RTKLIB\RTKLIB_bin-rtklib_2.4.3\RTKLIB_bin-rtklib_2.4.3\bin\CRX2RNX.exe")
+        command1 = r"{} {} -f".format(tool, file)
         # print(file)
         os.system(command1)
-        
-    command2 = "rm *gz"
-    os.system(command2)
+
+    # command2 = "rm *gz"
+    # os.system(command2)
 
 
-    
 def run_converting(root_path):
     binexes = [os.path.join(root_path, f) for f in os.listdir(root_path) if os.path.isdir(os.path.join(root_path, f))]
-    for folder in binexes:
+    for folder in binexes:  # [:1]:
+        print(folder)
         day_data_dir = os.path.join(folder, "hkks/1s")
         run_CRX2RNX(day_data_dir)
 
 
+input_ = r"C:\SzabolcsKelemen\PhD\GPS\HKKS\downloaded"
 
-
-
-input_ = r"/Users/kelemensz/Documents/Research/GPS/keep_from_qsync/HKG/HKKS_rinex/januar"
-# output = r"C:\SzabolcsKelemen\PhD\GPS\perth\december"
 
 run_converting(input_)
-
-
-
-
-
